4f38aadc54d905e96d14e6b3a7393bef460a0ebb
renakaagusta <renakaagusta28@gmail.com>
2022-06-27 14:32:21 +0700

    Add basic component

---
+++ macnivision/View/PetView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  macnivision
+//
+//  Created by renaka agusta on 27/06/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
